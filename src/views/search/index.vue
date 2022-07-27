b252bc107bb32a03cdc1178d6d8a6be567d0ed25
limu <1259567994@qq.com>
2022-07-27 17:06:56 +0800

    1

--- src/views/search/index.vue
+++ src/views/search/index.vue
@@ -1,13 +1,41 @@
 <template>
-  <div>sousuo </div>
+  <div>
+    <!-- 搜索框 -->
+    <div style="background-color: #4fc08d; width: 100%; height: 50px">
+      <van-search
+        v-model="value"
+        placeholder="请输入搜索关键词"
+        :label="address"
+        left-icon="arrow-down"
+      />
+      <van-popup v-model="isShow" position="bottom"
+        ><van-area
+          title="标题"
+          :area-list="areaList"
+          value="110101"
+          v-if="isShow"
+          @cancel="cancel"
+          @confirm="confirm"
+          class="area-l"
+      /></van-popup>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-name: 'AA'
-}
+  name: "AA",
+  data() {
+    return {
+      address: "北京",
+    };
+  },
+};
 </script>
 
 <style>
-
+.van-search {
+  left: 51px;
+  top: 8px;
+}
 </style>
