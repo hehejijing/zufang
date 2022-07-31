cc9013ac7f596ca1e26e424ad7173973def94ccd
limu <1259567994@qq.com>
2022-08-01 00:19:37 +0800

    我的收藏完成

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,5 @@
 import { swiper,group } from "./home";
-import { register,login,getInfo } from "./user";
+import { register,login,getInfo,getFavorites } from "./user";
 
 
 export const swiperApi = swiper
@@ -8,3 +8,4 @@ export const groupApi = group
 export const registerApi = register
 export const loginApi = login
 export const getInfoApi = getInfo
+export const getFavoritesApi = getFavorites
